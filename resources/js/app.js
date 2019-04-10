66beee1d9da3fb2e43050e38710683411e94f8c1
Fred <edwardfredparry@gmail.com>
2019-04-10 13:08:49 +0100

    1.05 - Google Analytics - Router afterEach

--- resources/js/app.js
+++ resources/js/app.js
@@ -68,3 +68,11 @@ const app = new Vue({
     render: h => h(App)
 });
 
+ga('set', 'page', router.currentRoute.path);
+ga('send', 'pageview');
+
+router.afterEach(( to, from ) => {
+    ga('set', 'page', to.path);
+    ga('send', 'pageview');
+});
+
